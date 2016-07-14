4b70f24d9013c30171ca6d53e28cebdbc286ec26
sokis <zengyi.zal@gmail.com>
2016-07-14 16:40:42 +0800

    新建blueprints目录，可以通过配合vuex-cli命令行工具快速创建项目结构

--- blueprints/component/index.js
+++ blueprints/component/index.js
@@ -2,11 +2,11 @@ import { normalizeCasing } from '../../src/util/text-helper';
 
 module.exports = {
   description() {
-    return 'Generates a dumb (aka Pure) component';
+    return '创建vue组件';
   },
   fileMapTokens() {
     return {
-      __style__: (options) => normalizeCasing(options.entity.name,'dashes')
+      __style__: (options) => normalizeCasing(options.entity.name, 'dashes')
     };
   }
 };
